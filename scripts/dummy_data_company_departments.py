986e918e08f0085833cf3b23df632a17bf8d6b03
Dragos <donotwriteme@mail.com>
2024-01-24 17:42:14 +0200

    fix insert company_departments script

--- scripts/dummy_data_company_departments.py
+++ scripts/dummy_data_company_departments.py
@@ -8,6 +8,18 @@ from src.db import cursor
 schema_name = os.getenv('SCHEMA_NAME', 'inexistent_schema')
 
 
+def insert_department(departments):
+    cursor.fast_executemany = True
+    cursor.executemany(
+        (
+            f'insert into [{schema_name}].department '
+            '(department_name, department_description) values (?,?)'
+        ),
+        departments,
+    )
+    cursor.commit()
+
+
 if __name__ == '__main__':
     faker = Faker()
     END_RANGE = 1000
@@ -23,8 +35,8 @@ if __name__ == '__main__':
         'Marketing',
         'Sales',
         'IT',
-        'ResearchAndDevelopment',
-        'CustomerService',
+        'Research and Development',
+        'Customer Service',
         'Purchasing',
         'Legal',
         'Production',
@@ -34,30 +46,30 @@ if __name__ == '__main__':
         'Engineering',
     ]
 
+    # TODO do not allow insert duplicate departments
     for department in company_departments:
-        departments.append((department, 'Lorem Ipsum'))
+        departments.append((department, f'{department} Description'))
 
-    cursor.fast_executemany = True
-    cursor.executemany(
-        (
-            f'insert into [{schema_name}].department '
-            '(department_name, department_description) values (?,?)'
-        ),
-        departments,
-    )
-    cursor.commit()
+    # insert_department(departments)
+    del departments
+
     department_id = cursor.execute(
         f'select id from [{schema_name}].department'
     ).fetchall()
 
     start_random = len(company_departments) // 2
     end_random = len(company_departments)
+
     for company_id in companies_id:
         company_departments = []
         for _ in range(random.randint(start_random, end_random)):
-            company_departments.append(
-                (company_id[0], random.choice(department_id)[0])
+            company_department = (
+                company_id[0],
+                random.choice(department_id)[0],
             )
+            if company_department not in company_departments:
+                company_departments.append(company_department)
+
         cursor.fast_executemany = True
         cursor.executemany(
             (
@@ -66,5 +78,6 @@ if __name__ == '__main__':
             ),
             company_departments,
         )
+        cursor.commit()
 
     cursor.close()
